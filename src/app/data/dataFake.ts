fe94090ae785aa3aba010b897be3d39e5063fd77
mpercinio <mpercinio@gmail.com>
2023-10-22 13:53:59 -0300

    angularblog

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Bilheteria de mais de um milhão de dolares",
+    "description": "Grande e ainda com participação de Val Kilmer.",
+    "photoCover": "https://classic.exame.com/wp-content/uploads/2022/10/tom-cruise-1.jpg?quality=70&strip=info&w=1024"
+  },
+  {
+    "id": "2",
+    "title": "Lançamento foi em julho de 2023",
+    "description": "O publico gostou muito do filme",
+    "photoCover": "https://res.cloudinary.com/nerd-club/image/upload/c_thumb,dpr_2.0,f_auto,h_1000,q_auto:good,w_1400/Miss%C3%A3o-Imposs%C3%ADvel-7.jpg"
+  },
+  {
+    "id": "3",
+    "title": "Lançamento vai ser no ano de 2024",
+    "description": "O publico com muita vontade para assistir o termino desta história",
+    "photoCover": "https://cinepop.com.br/wp-content/uploads/2023/07/missao-impossivel.jpg.webp"
+  },
+  {
+    "id": "4",
+    "title": "Não vai existir filme da série Friends",
+    "description": "O publico lamenta essa decisão",
+    "photoCover": "https://www.revistaeletricidade.com.br/wp-content/uploads/2021/05/friendsreunion2.png"
+  }
+]
